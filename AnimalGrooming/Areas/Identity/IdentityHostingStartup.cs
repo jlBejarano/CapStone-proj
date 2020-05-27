23f48752ce873d39b449e9790e7b00fca31eb5a8
jorge bejarano <jorge.bejarano@yahoo.com>
2020-05-27 12:30:33 -0400

    Added customer controller and action filters

---
+++ AnimalGrooming/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AnimalGrooming.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AnimalGrooming.Areas.Identity.IdentityHostingStartup))]
+namespace AnimalGrooming.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
